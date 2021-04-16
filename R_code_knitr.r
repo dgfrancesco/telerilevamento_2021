df459c359d81191b68da841f31b51fa41d45e4ef
dgfrancesco <80513591+dgfrancesco@users.noreply.github.com>
2021-04-16 09:32:44 +0200

    Create R_code_knitr.r

---
+++ R_code_knitr.r
@@ -0,0 +1,7 @@
+# R_code_knitr.r
+
+setwd("~/lab/greenland") # Linux
+# setwd("C:/lab/greenland") # Windows
+# setwd("/Users/name/Desktop/lab/greenland") # Mac 
+
+library(knitr)  # oppure require("knitr")
