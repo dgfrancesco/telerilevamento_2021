3c1db20a5a63398ebf148aa16bebf9cf6c389a32
dgfrancesco <80513591+dgfrancesco@users.noreply.github.com>
2021-04-16 11:34:40 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -1,5 +1,8 @@
 # R_code_knitr.r
 
+#install.packages("knitr")
+#install.packages("tinytex")
+
 setwd("~/lab/greenland") # Linux
 # setwd("C:/lab/greenland") # Windows
 # setwd("/Users/name/Desktop/lab/greenland") # Mac 
@@ -8,3 +11,6 @@ library(knitr)  # oppure require("knitr")
 
 #stitch("~/Downloads/R_code_greenland.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
 stitch("C:/lab/R_code_greenland.r.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+
+tinytex::install_tinytex()
+tinytex::tlmgr_update()
