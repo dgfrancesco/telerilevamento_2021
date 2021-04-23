38a572c652c3759a4e91a60ea190768bbbe3acb6
Francesco Di Grazia <80513591+dgfrancesco@users.noreply.github.com>
2021-04-23 10:01:13 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -6,7 +6,7 @@ setwd("~/lab/") # Linux
 # setwd("C:/lab/") # Windows
 # setwd("/Users/name/Desktop/lab/") # Mac 
 
-so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg") #create a rasterbrick objects
+so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg") #create a rasterbrick objects, si caricano più livelli insieme RGB
 so
 
 #visualize RGB levels
@@ -25,9 +25,31 @@ plot(soc$map,col=cl)
 soe <- unsuperClass(so, nClasses=20)
 plot(soe$map)
 
-#Doenload an image from: https://www.esa.int/ESA_Multimedia/Missions/Solar_Orbiter/(result_type)/images
+#Download an image from: https://www.esa.int/ESA_Multimedia/Missions/Solar_Orbiter/(result_type)/images
 sun <- brick("Sun.png") 
 # Unsupervised Classification
 set.seed(42)  # questa funzione serve per avere sempre le stesse repliche per fare il modello
 sunc <- unsuperClass(sun, nClasses=3)
 plot(sunc$map)
+
+### Day xx ###
+#Grand Canyon
+#Download an image from: https://landsat.visibleearth.nasa.gov/view.php?id=80948
+library(raster)
+library(RStoolbox)
+
+setwd("~/lab/") # Linux
+# setwd("C:/lab/") # Windows
+# setwd("/Users/name/Desktop/lab/") # Mac
+
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+plotRGB(gc, r=1, g=2, b=3, stretch="lin") 
+plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+
+gcc2 <- unsuperClass(gc, nClasses=2)
+gcc2
+plot(gcc2$map)
+
+gcc4 <- unsuperClass(gc, nClasses=4)
+gcc4
+plot(gcc4$map)
